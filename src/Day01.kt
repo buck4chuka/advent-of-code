3577171e5319c3110ac023594a9d9fcad35a3e99
Chuka Nwobi <chuka.nwobi@greenhouse.io>
2024-12-03 10:53:07 -0500

    Day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,29 @@
+import kotlin.math.abs
+
 fun main() {
+    fun toLeftRight(input: List<String>): Pair<List<Int>, List<Int>> {
+        return input.foldRight(Pair(listOf(), listOf())) { line, acc ->
+            val (left, right) =  line.split("\\s+".toRegex()).map { it.toInt() }
+            Pair(acc.first + left, acc.second + right)
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+       val(left,right) = toLeftRight(input)
+        return left.sorted().zip(right.sorted()).fold(0) { acc, (l, r) ->
+            acc + abs( l - r)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val(left,right) = toLeftRight(input)
+        val rFreqCount = right.groupingBy { it }.eachCount()
+        return left.sumOf { (rFreqCount[it] ?: 0) * it }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("year2024/day01/Day01")
     part1(input).println()
     part2(input).println()
 }
