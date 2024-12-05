321af1c45d2338a201e6f26fdb23fa695ae496ae
Chuka Nwobi <chuka.nwobi@greenhouse.io>
2024-12-05 00:04:16 -0500

    Day 4 completed

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,5 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T, U> Iterable<T>.cartesianProduct(other: Iterable<U>): List<Pair<T, U>> = this.flatMap { other.map { b -> it to b } }
